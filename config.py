fc71620966e87837dd3e48e715ca6fa264cff347
Payal987-blip <kiteravi01@gmail.com>
2025-10-02 23:00:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = os.environ.get("API_HASH","921b2c09a090b6848f35798dcc750d5a")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@canonbook_bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@CornBetaBot")
 BOT_TEXT = "Jordan_Extractor"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "8433080835"))
